0fe95085d74c15d0d173450997309fb456f39ae2
RustamAleev2023 <ridermix@yandex.ru>
2023-11-13 15:02:47 +0200

    Task2 done

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        task1();
-
+//        task1();
+        task2();
     }
 
     //Task1
@@ -14,8 +16,6 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите число для вывода последовательности Фибоначчи");
         int n = scanner.nextInt();
-
-
         int[] f = new int[n];
 
         f[0] = 0;
@@ -28,6 +28,26 @@ public class Main {
         for (int i = 0; i < f.length; i++) {
             System.out.println(f[i]);
         }
+    }
 
+    //Task2
+    public static void task2() {
+        int n = 1_000_000;
+
+        for (int i = 2; i <= n ; i++) {
+            boolean isPrimeNumber = true;
+            double squareRoot = Math.sqrt(i) + 2;
+            int j = 2;
+            while (j <= squareRoot && isPrimeNumber){
+                if(i % j == 0){
+                    isPrimeNumber = false;
+                }
+                j++;
+            }
+            if(isPrimeNumber){
+                System.out.println(i);
+            }
+
+        }
     }
 }
