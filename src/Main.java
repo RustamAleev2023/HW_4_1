a6ee962da0e973a4bf1c80c5e23d337ced24650f
RustamAleev2023 <ridermix@yandex.ru>
2023-11-14 12:46:51 +0200

    Task1 refactored

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -7,26 +8,43 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 //        task1();
-        task2();
+//        task2();
+//        task3();
+
+//        int i = 153;
+//            //разбиваем число на разряды
+//            int[] digits = new int[String.valueOf(i).length()];
+//            int n = i;
+//            for (int j = 0; j < digits.length; j++) {
+//                digits[j] = n % 10;
+//                n = n / 10;
+//                System.out.println(digits[j]);
+//            }
+//            int[] result = new int[digits.length];
+//        System.out.println(result.length);
+//            for (int j = 0; j < result.length; j++) {
+//
+//                result[j] = (int) Math.pow(digits[j],1/digits.length);
+//                System.out.println(result[j]);
+//            }
+
+
     }
 
     //Task1
     //Fibonacci numbers
     public static void task1() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите число для вывода последовательности Фибоначчи");
-        int n = scanner.nextInt();
-        int[] f = new int[n];
 
-        f[0] = 0;
-        f[1] = 1;
+        int maxLimit = 10_000_000;
+        int a = 0;
+        int b = 1;
+        int c;
 
-        for (int i = 2; i < n; i++) {
-            f[i] = f[i - 1] + f[i - 2];
-        }
-
-        for (int i = 0; i < f.length; i++) {
-            System.out.println(f[i]);
+        while (a <= maxLimit){
+            System.out.println(a);
+            c = a + b;
+            a = b;
+            b = c;
         }
     }
 
@@ -34,20 +52,43 @@ public class Main {
     public static void task2() {
         int n = 1_000_000;
 
-        for (int i = 2; i <= n ; i++) {
+        for (int i = 2; i <= n; i++) {
             boolean isPrimeNumber = true;
             double squareRoot = Math.sqrt(i) + 2;
             int j = 2;
-            while (j <= squareRoot && isPrimeNumber){
-                if(i % j == 0){
+            while (j <= squareRoot && isPrimeNumber) {
+                if (i % j == 0) {
                     isPrimeNumber = false;
                 }
                 j++;
             }
-            if(isPrimeNumber){
+            if (isPrimeNumber) {
                 System.out.println(i);
             }
 
         }
+    }
+
+    //Task3
+    public static void task3() {
+        int min = 10;
+        int max = 1_000_000;
+
+
+        for (int i = min; i <= max; i++) {
+            //разбиваем число на разряды
+            int[] digits = new int[String.valueOf(i).length()];
+            int n = i;
+            for (int j = 0; j < digits.length; j++) {
+                digits[j] = n % 10;
+                n = n / 10;
+            }
+            int[] result = new int[digits.length];
+            for (int j = 0; j < result.length; j++) {
+                result[j] = (int) Math.pow(digits[j], (1 / digits.length));
+            }
+        }
+
+
     }
 }
